91b6b86689095f061325c55379695c370d8110bc
Joshua Samberg <josh@shelby.tv>
2013-09-25 17:12:20 -0400

    Created scripts for exporting and uploading the three collections we use now;query is bogus,need to fix

---
+++ users.sh
@@ -0,0 +1,5 @@
+date=$(date +%Y-%m-%d)
+exportFilename="$date"-users.json
+echo "$exportFilename"
+mongoexport -u gt_admin -p Ov3rt1m3#4# --authenticationDatabase admin -d nos-production -c users --host nos-db-s0-b:27018 --slaveOk 1 -o "$exportFilename" -q "{ '_id' : '$date' }" -f roll_followings,aa,ab,ad,ae,af,ac,ag,as,authentications,nickname,downcase_nickname,current_sign_in_at,last_sign_in_at,bb
+s3cmd --progress --multipart-chunk-size-mb=100 put "$exportFilename" s3://dev-shelby-mortar-share/input/users/"$date"/users.json
