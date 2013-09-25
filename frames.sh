058a1ba140be4543a2ef23ec0fa830ce310c591a
Joshua Samberg <josh@shelby.tv>
2013-09-25 19:41:53 -0400

    Temporarily limiting the export scripts to one record each for testing

--- frames.sh
+++ frames.sh
@@ -1,4 +1,4 @@
 date=$(date +%Y-%m-%d)
 exportFilename="$date"-frames.json
-mongoexport -u gt_admin -p Ov3rt1m3#4# --authenticationDatabase admin -d gt-roll-frame -c frames --host gt-db-roll-frame-s0-c --slaveOk 1 -o "$exportFilename" -q "{ '_id' : '$date' }"
+mongoexport -u gt_admin -p Ov3rt1m3#4# --authenticationDatabase admin -d gt-roll-frame -c frames --host gt-db-roll-frame-s0-c --slaveOk 1 -o "$exportFilename" -q "{ '_id' : { '\$oid' : '4f9393169a725b07de000014'} }"
 s3cmd --progress --multipart-chunk-size-mb=100 put "$exportFilename" s3://dev-shelby-mortar-share/input/frames/"$date"/frames.json
--- videos.sh
+++ videos.sh
@@ -1,5 +1,5 @@
 date=$(date +%Y-%m-%d)
 exportFilename="$date"-videos.json
 echo "$exportFilename"
-mongoexport -u gt_admin -p Ov3rt1m3#4# --authenticationDatabase admin -d gt-video -c videos --host gt-db-video-s0-b --slaveOk 1 -o "$exportFilename" -q "{ '_id' : '$date' }"
+mongoexport -u gt_admin -p Ov3rt1m3#4# --authenticationDatabase admin -d gt-video -c videos --host gt-db-video-s0-b --slaveOk 1 -o "$exportFilename" -q "{ '_id' : { '\$oid' : '4f61f781b415cc5eff000001'} }"
 s3cmd --progress --multipart-chunk-size-mb=100 put "$exportFilename" s3://dev-shelby-mortar-share/input/videos/"$date"/videos.json
