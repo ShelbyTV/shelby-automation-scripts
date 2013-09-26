7fdd3f89dd851a787d63eed168283ec3b476105e
Joshua Samberg <josh@shelby.tv>
2013-09-26 09:46:56 -0400

    RESTARTABILITY! If the export file already exists, skip the export and go straight to the upload

--- frames.sh
+++ frames.sh
@@ -1,6 +1,10 @@
 set -e
 
-# date=$(date +%Y-%m-%d)
+date=${date-$(date +%Y-%m-%d)}
 exportFilename="$date"-frames.json
-mongoexport -u gt_admin -p Ov3rt1m3#4# --authenticationDatabase admin -d gt-roll-frame -c frames --host gt-db-roll-frame-s0-c --slaveOk 1 -o "$exportFilename"
+# if the export file doesn't already exist, do the export, otherwise go straight to the upload
+if [ ! -e $exportFilename ]
+then
+  mongoexport -u gt_admin -p Ov3rt1m3#4# --authenticationDatabase admin -d gt-roll-frame -c frames --host gt-db-roll-frame-s0-c --slaveOk 1 -o "$exportFilename"
+fi
 s3cmd --progress --multipart-chunk-size-mb=100 put "$exportFilename" s3://dev-shelby-mortar-share/input/frames/"$date"/frames.json
--- videos.sh
+++ videos.sh
@@ -1,6 +1,10 @@
 set -e
 
-# date=$(date +%Y-%m-%d)
+date=${date-$(date +%Y-%m-%d)}
 exportFilename="$date"-videos.json
-mongoexport -u gt_admin -p Ov3rt1m3#4# --authenticationDatabase admin -d gt-video -c videos --host gt-db-video-s0-b --slaveOk 1 -o "$exportFilename"
+# if the export file doesn't already exist, do the export, otherwise go straight to the upload
+if [ ! -e $exportFilename ]
+then
+  mongoexport -u gt_admin -p Ov3rt1m3#4# --authenticationDatabase admin -d gt-video -c videos --host gt-db-video-s0-b --slaveOk 1 -o "$exportFilename"
+fi
 s3cmd --progress --multipart-chunk-size-mb=100 put "$exportFilename" s3://dev-shelby-mortar-share/input/videos/"$date"/videos.json
